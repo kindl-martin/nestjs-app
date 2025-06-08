80797cf8fa8e64cb864d6b20838bce306d966bdb
Martin Kindl <kindl.m@icloud.com>
2025-06-08 10:03:34 +0200

    add flyio secrets

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0', () =>
+    console.log('Server is running on 0.0.0.0:3000'),
+  );
 }
+
 bootstrap().catch((err) => console.error(err));
